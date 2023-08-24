e5ab5ee19a8344231574728b3cf33616510403eb
Roy Cohen <misteroy@gmail.com>
2023-08-24 14:54:54 +0300

    Modified ptlx-create-proj script

--- ptlx-create-proj.sh
+++ ptlx-create-proj.sh
@@ -2,4 +2,7 @@
 
 #PetaLinux SD Card utils script
 #original version by Roy Cohen (bspguy.com)
+PTLX_PROJ_ROOT=/../../
+PTLX_IMAGES=${PTLX_PROJ_ROOT}/images/linux/
+source ${PETALINUX}/settings.sh
 petalinux-create -t project  -s ../xilinx-zcu102-v2022.2-10141622.bsp 
